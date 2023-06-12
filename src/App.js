bf2a521aaa3163771fb5cfcddf2ceec7e5e575b5
Prajwal <praju.anjanappa@gmail.com>
2023-06-12 22:10:03 +0530

    content added

--- src/App.js
+++ src/App.js
@@ -10,6 +10,12 @@ import ReadArticle from "./pages/readArticle";
 import Contact from "./pages/contact";
 import Notfound from "./pages/404";
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
 import { TRACKING_ID } from "./data/tracking";
 import "./app.css";
 
--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -39,21 +39,17 @@ const Projects = () => {
 					</div>
 					<div className="projects-container">
 						<div className="title projects-title">
-							Things I’ve made trying to put my dent in the
-							universe.
+							Showcasing the Power of Innovation and Creativity
 						</div>
 
 						<div className="subtitle projects-subtitle">
-							I've worked on a variety of projects over the years
-							and I'm proud of the progress I've made. Many of
-							these projects are open-source and available for
-							others to explore and contribute to. If you're
-							interested in any of the projects I've worked on,
-							please feel free to check out the code and suggest
-							any improvements or enhancements you might have in
-							mind. Collaborating with others is a great way to
-							learn and grow, and I'm always open to new ideas and
-							feedback.
+							I proudly present a curated selection of my work
+							that embodies the power of innovation and
+							creativity. Each project represents a unique
+							challenge, a story of problem-solving, and a
+							testament to my commitment to delivering exceptional
+							results. Take a closer look at some of my notable
+							endeavors below.
 						</div>
 
 						<div className="projects-list">
